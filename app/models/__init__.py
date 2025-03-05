c57ca368f7c589eda442c9f1081b7089e4b6da91
KyberDot <129681959+KyberDot@users.noreply.github.com>
2025-03-05 00:57:58 +0000

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Import models to make them available at the package level
-from .user import User
-from .court import Court
-from .booking import Booking
-from .transaction import Transaction
-from .voucher import Voucher, VoucherUsage
-from .pricing_rule import PricingRule
--- app/services/__init__.py
+++
@@ -1,5 +0,0 @@
-# Services package initialization
-from .booking_service import admin_required, court_manager_required, create_booking, cancel_booking
-from .pricing_service import calculate_price
-from .email_service import send_booking_confirmation, send_password_reset_email
-from .payment_service import create_paypal_order, capture_paypal_payment, refund_payment
